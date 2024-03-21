99d3983f0432568a2b42ad6967bd32d1c660e7c1
FerdisJ <161576868+FerdisJ@users.noreply.github.com>
2024-03-21 08:50:24 +0100

    Create +18.py
    A +18 filter for pages in python

---
+++ +18.py
@@ -0,0 +1,12 @@
+permatrago = int(input("Dime tu edad: "))
+if permatrago < 18:
+  #Impide el acceso
+    print("Acceso denegado, solo +18")
+elif permatrago >= 18:
+  #Redirecciona a la pagina 
+    print("Bienvenido")
+else:
+  #En caso de cualquier tipo de error saldira este mensaje
+    print("Error en el sistema")
+
+print("Fin de la matraca cara nalga")
