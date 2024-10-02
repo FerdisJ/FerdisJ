3aa9bbc9f1cee2b98e48619bf8e907c9c7b07aee
FerdisJ <161576868+FerdisJ@users.noreply.github.com>
2024-10-02 12:28:51 +0200

    Update +18.py

--- +18.py
+++ +18.py
@@ -1,12 +1,1 @@
-permatrago = int(input("Dime tu edad: "))
-if permatrago < 18:
-  #Impide el acceso
-    print("Acceso denegado, solo +18")
-elif permatrago >= 18:
-  #Redirecciona a la pagina 
-    print("Bienvenido")
-else:
-  #En caso de cualquier tipo de error saldira este mensaje
-    print("Error en el sistema")
 
-print("Fin del programa")
